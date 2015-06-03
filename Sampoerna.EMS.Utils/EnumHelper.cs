ec0bf01617153088e78c50c760f226c319cf1415
irman <irman@voxteneo.asia>
2015-06-03 16:02:05 +0700

    add project Sampoerna.EMS.AutoMapperExtensions and Sampoerna.EMS.Utils
    move some class to related project

---
+++ Sampoerna.EMS.Utils/EnumHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Sampoerna.EMS.Utils
+{
+    public static class EnumHelper
+    {
+        public static string GetDescription(Enum en)
+        {
+            Type type = en.GetType();
+
+            MemberInfo[] memInfo = type.GetMember(en.ToString());
+
+            if (memInfo != null && memInfo.Length > 0)
+            {
+                object[] attrs = memInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attrs != null && attrs.Length > 0)
+                {
+                    return ((DescriptionAttribute)attrs[0]).Description;
+                }
+            }
+
+            return en.ToString();
+        }
+
+
+        /// <summary>
+        /// Filter the enum based on an attribute
+        /// </summary>
+        /// <typeparam name="TEnum">The type of the enum.</typeparam>
+        /// <typeparam name="TAttribute">The type of the attribute.</typeparam>
+        /// <returns></returns>
+        public static IEnumerable<TEnum> FilterEnumWithAttributeOf<TEnum, TAttribute>()
+            where TEnum : struct
+            where TAttribute : class
+        {
+            foreach (var field in
+                typeof(TEnum).GetFields(BindingFlags.GetField |
+                                         BindingFlags.Public |
+                                         BindingFlags.Static))
+            {
+                if (field.GetCustomAttributes(typeof(TAttribute), false).Length > 0)
+                    yield return (TEnum)field.GetValue(null);
+            }
+        }
+    }
+}
