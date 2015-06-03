7f106d61a43bb540a7095fc6b4f3476297068ec4
irman <irman@voxteneo.asia>
2015-06-03 14:29:29 +0700

    add some reference and some necessary class

---
+++ Sampoerna.EMS.BusinessObject/Inputs/UserInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sampoerna.EMS.BusinessObject.Inputs
+{
+    public class UserInput
+    {
+        public string USERNAME { get; set; }
+        public string FIRST_NAME { get; set; }
+        public string LAST_NAME { get; set; }
+        public bool? IS_ACTIVE { get; set; }
+        public int? MANAGER_ID { get; set; }
+    }
+}
