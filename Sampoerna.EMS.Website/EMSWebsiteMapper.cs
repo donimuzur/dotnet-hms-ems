86626d18131e5d8ef533fffb28d5d84938750c89
a.siskayana <a.siskayana@voxteneo.asia>
2015-06-05 10:44:48 +0700

    add mapper Compan

--- Sampoerna.EMS.Website/EMSWebsiteMapper.cs
+++ Sampoerna.EMS.Website/EMSWebsiteMapper.cs
@@ -11,6 +11,21 @@ namespace Sampoerna.EMS.Website
         {
             //AutoMapper
             Mapper.CreateMap<USER, UserViewModel>().IgnoreAllNonExisting();
+
+            //Company
+            Mapper.CreateMap<T1001, CompanyViewModel>().IgnoreAllNonExisting()
+            .ForMember(dest => dest.CompanyId, opt => opt.MapFrom(src => src.COMPANY_ID))
+            .ForMember(dest => dest.DocumentBukrs, opt => opt.MapFrom(src => src.BUKRS))
+            .ForMember(dest => dest.DocumentBukrstxt, opt => opt.MapFrom(src => src.BUKRSTXT))
+            .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => src.CREATED_DATE)); 
+
+            //Company
+            Mapper.CreateMap<CompanyViewModel, T1001>().IgnoreAllNonExisting()
+            .ForMember(dest => dest.COMPANY_ID, opt => opt.MapFrom(src => src.CompanyId))
+            .ForMember(dest => dest.BUKRS, opt => opt.MapFrom(src => src.DocumentBukrs))
+            .ForMember(dest => dest.BUKRSTXT, opt => opt.MapFrom(src => src.DocumentBukrstxt))
+            .ForMember(dest => dest.CREATED_DATE, opt => opt.MapFrom(src => src.CreatedDate)); 
+
         }
     }
 }
