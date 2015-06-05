e95591d63cdbf312c158e15fa624295a28b0f6e0
irman <irman@voxteneo.asia>
2015-06-05 14:11:30 +0700

    add BaseController

---
+++ Sampoerna.EMS.Website/Controllers/BaseController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sampoerna.EMS.Website.Controllers
+{
+
+    public class BaseController : Controller
+    {
+        
+        protected ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        private bool IsAvailableDistrictCookie(string cookieName)
+        {
+            if (ControllerContext.HttpContext.Request.Cookies.AllKeys.Contains(cookieName))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        protected void CreateCookie(string cookieName, string district)
+        {
+            HttpCookie cookie;
+            if (!IsAvailableDistrictCookie(cookieName))
+            {
+                cookie = new HttpCookie(cookieName);
+
+
+            }
+            else
+            {
+                cookie = ControllerContext.HttpContext.Request.Cookies[cookieName];
+
+            }
+            if (cookie != null)
+            {
+                cookie.Value = district;
+                cookie.Expires = DateTime.Now.AddYears(1);
+                ControllerContext.HttpContext.Response.Cookies.Add(cookie);
+            }
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var viewResult = filterContext.Result as ViewResult;
+            if (viewResult == null)
+                return;
+
+            //if (ConfigurationManager.AppSettings["BaseUrl"] != null)
+            //{
+            //    viewResult.ViewBag.BaseUrl = ConfigurationManager.AppSettings["BaseUrl"].ToString();
+            //}
+
+            //var descriptor = filterContext.ActionDescriptor;
+            //var actionName = descriptor.ActionName;
+            //var controllerName = descriptor.ControllerDescriptor.ControllerName;
+
+            //_accountBll.InsertLog(controllerName, actionName, CurrentUser
+            //    );
+
+            base.OnActionExecuted(filterContext);
+
+        }
+
+    }
+}
