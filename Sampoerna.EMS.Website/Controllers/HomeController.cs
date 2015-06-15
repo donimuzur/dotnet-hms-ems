64df87d75abde3b4d9b8042577cd38c003be33ed
adnan setiawan <adnan@voxteneo.asia>
2015-06-15 11:11:23 +0700

    remove ck4c bll from home->index

--- Sampoerna.EMS.Website/Controllers/HomeController.cs
+++ Sampoerna.EMS.Website/Controllers/HomeController.cs
@@ -12,26 +12,16 @@ namespace Sampoerna.EMS.Website.Controllers
     {
         private ICompanyBLL _companyBll;
         private IWorkflowBLL _workflowBll;
-        private ICK4C_BLL _ck4CBll;
-        public HomeController(ICompanyBLL companyBll, IWorkflowBLL workflowBll, ICK4C_BLL ck4CBll)
+        
+        public HomeController(ICompanyBLL companyBll, IWorkflowBLL workflowBll)
         {
             _companyBll = companyBll;
             _workflowBll = workflowBll;
-            _ck4CBll = ck4CBll;
+            
         }
 
         public ActionResult Index()
         {   
-          CK4C ck4c = new CK4C();
-            ck4c.CK4C_NUMBER = "cccccc";
-            ck4c.COMPANY_ID = 101;
-            ck4c.PLANT_ID = 1;
-            List<CK4C_ITEM> items = new List<CK4C_ITEM>();
-           CK4C_ITEM item = new CK4C_ITEM();
-            item.PRODUCED_QTY = 1000;
-            items.Add(item);
-            ck4c.CK4C_ITEM = items;
-            _ck4CBll.Insert(ck4c);
            return View();
         }
         
