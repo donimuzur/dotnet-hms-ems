7a25658228d953907a5e037ebe4690b7367f7fb9
irman <irman@voxteneo.asia>
2015-06-17 15:50:28 +0700

    done view open document list

--- Sampoerna.EMS.Website/Controllers/PBCK1Controller.cs
+++ Sampoerna.EMS.Website/Controllers/PBCK1Controller.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using AutoMapper;
+using Microsoft.Ajax.Utilities;
 using Sampoerna.EMS.BusinessObject.Inputs;
 using Sampoerna.EMS.Contract;
 using Sampoerna.EMS.Core;
@@ -20,7 +21,7 @@ namespace Sampoerna.EMS.Website.Controllers
         {
             _pbck1Bll = pbckBll;
         }
-        
+
         private List<PBCK1Item> GetPBCKItems(PBCK1FilterViewModel filter = null)
         {
             if (filter == null)
@@ -32,20 +33,17 @@ namespace Sampoerna.EMS.Website.Controllers
             var input = Mapper.Map<PBCK1Input>(filter);
             return Mapper.Map<List<PBCK1Item>>(_pbck1Bll.GetPBCK1ByParam(input));
         }
-        
-        private SelectList GetYearList()
+
+        private SelectList GetYearList(List<PBCK1Item> pbck1Data)
         {
-            int currentYear = DateTime.Now.Year;
-            var selectItemSource = new List<SelectItemModel>();
-            for (var i = 0; i < 5; i++)
-            {
-                selectItemSource.Add(new SelectItemModel()
-                {
-                    ValueField = (currentYear - i),
-                    TextField = (currentYear - i).ToString()
-                });
-            }
-            return new SelectList(selectItemSource, "ValueField", "TextField");
+            var query = from x in pbck1Data
+                        where x.PERIOD_FROM.HasValue
+                        select new SelectItemModel()
+                        {
+                            ValueField = x.PERIOD_FROM.Value.Year,
+                            TextField = x.PERIOD_FROM.Value.Year.ToString()
+                        };
+            return new SelectList(query.DistinctBy(c => c.ValueField), "ValueField", "TextField");
         }
 
         //
@@ -61,11 +59,11 @@ namespace Sampoerna.EMS.Website.Controllers
 
         public ActionResult IndexInitial(PBCK1ViewModel model)
         {
-            model.SearchInput.YearList = GetYearList();
             model.SearchInput.NPPBKCIDList = GlobalFunctions.GetNppbkcAll();
             model.SearchInput.CreatorList = GlobalFunctions.GetCreatorList();
             model.SearchInput.POAList = new SelectList(new List<SelectItemModel>(), "ValueField", "TextField");
             model.Details = GetPBCKItems();
+            model.SearchInput.YearList = GetYearList(model.Details);
             return View("Index", model);
         }
 
@@ -78,6 +76,16 @@ namespace Sampoerna.EMS.Website.Controllers
                 Detail = null
             };
             return View(model);
+        }
+
+        public ActionResult Edit(long id)
+        {
+            return View(new PBCK1ItemViewModel(){ MainMenu = Enums.MenuList.ExcisableGoodsMovement, CurrentMenu = PageInfo });
+        }
+
+        public ActionResult Details(long id)
+        {
+            return View(new PBCK1ItemViewModel() { MainMenu = Enums.MenuList.ExcisableGoodsMovement, CurrentMenu = PageInfo });
         }
 
         [HttpPost]
