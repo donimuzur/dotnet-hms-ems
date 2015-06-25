9b447df682a25477784970f9a684456394dd5f0b
adnan setiawan <adnan@voxteneo.asia>
2015-06-25 11:17:08 +0700

    update edmx, move scripts validate to layout master

--- Sampoerna.EMS.DAL/EMSEntities.Context.cs
+++ Sampoerna.EMS.DAL/EMSEntities.Context.cs
@@ -83,5 +83,7 @@ namespace Sampoerna.EMS.BusinessObject
         public virtual DbSet<ZAIDM_EX_SERIES> ZAIDM_EX_SERIES { get; set; }
         public virtual DbSet<ZAIDM_POA_MAP> ZAIDM_POA_MAP { get; set; }
         public virtual DbSet<EX_GROUP_TYPE> EX_GROUP_TYPE { get; set; }
+        public virtual DbSet<EMAIL_TEMPLATE> EMAIL_TEMPLATE { get; set; }
+        public virtual DbSet<WORKFLOW_EMAIL> WORKFLOW_EMAIL { get; set; }
     }
 }
