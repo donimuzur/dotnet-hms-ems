1496a79940b49bbe982db4c23b9197d9765e1ade
a.siskayana <a.siskayana@voxteneo.asia>
2015-06-25 11:18:32 +0700

    work in progress

--- Sampoerna.EMS.Website/Models/PLANT/PlantViewModel.cs
+++ Sampoerna.EMS.Website/Models/PLANT/PlantViewModel.cs
@@ -18,37 +18,47 @@ namespace Sampoerna.EMS.Website.Models.PLANT
     }
     public class PlantFormModel : BaseModel
     {
-        
+
         public IEnumerable<SelectListItem> PlantIdListItems { get; set; }
         public IEnumerable<SelectListItem> Nppbkc { get; set; }
         public IEnumerable<SelectListItem> RecieveMaterialListItems { get; set; }
-        public  DetailPlantT1001W Detail { get; set; }
+        public DetailPlantT1001W Detail { get; set; }
     }
 
     public class DetailPlantT1001W
     {
-       
+
         public long PlantId { get; set; }
+
+        [Required(ErrorMessage = "please fill this field")]
+        [Display(Name = "Werks")]
         public string Werks { get; set; }
         public string PlantDescription { get; set; }
 
         public bool IsMainPlant { get; set; }
 
-        [Required(ErrorMessage = "Please Insert Address")]
+        [Required(ErrorMessage = "please fill this field")]
         [Display(Name = "Address")]
         public string Address { get; set; }
 
-        [Required(ErrorMessage = "Please Insert City")]
+        [Required(ErrorMessage = "please fill this field")]
         [Display(Name = "City")]
         [StringLength(50, ErrorMessage = "Max Lenght : 50")]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "please fill this field")]
+        [Display(Name = "Skeptis")]
+        [StringLength(50, ErrorMessage = "Max Lenght : 50")]
         public string Skeptis { get; set; }
+
+        [Required(ErrorMessage = "please fill this field")]
+        [Display(Name = "Skeptis")]
         public int? RecievedMaterialTypeId { get; set; }
         public string NPPBKC_NO { get; set; }
         public string KPPBC_NO { get; set; }
         public ZAIDM_EX_NPPBKC NPPBKC { get; set; }
 
-        public ZAIDM_EX_GOODTYP GOODTYP { get; set; }   
+        public ZAIDM_EX_GOODTYP GOODTYP { get; set; }
     }
 
 }
