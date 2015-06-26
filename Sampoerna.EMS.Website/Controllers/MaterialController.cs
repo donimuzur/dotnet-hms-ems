828916f57de19c99acb1ec30f8382698511f4091
Deky <deky@voxteneo.asia>
2015-06-26 11:28:00 +0700

    MaterialController

---
+++ Sampoerna.EMS.Website/Controllers/MaterialController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using Sampoerna.EMS.Contract;
+using Sampoerna.EMS.Core;
+
+namespace Sampoerna.EMS.Website.Controllers
+{
+    public class MaterialController : BaseController
+    {
+
+        public MaterialController(IPageBLL pageBLL)
+            : base(pageBLL, Enums.MenuList.MasterData)
+        {
+           
+        }
+
+
+        //
+        // GET: /Material/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
