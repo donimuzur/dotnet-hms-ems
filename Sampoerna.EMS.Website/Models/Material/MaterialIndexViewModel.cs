44d88d4eefffcaf9f3788e21fb74759643574757
Deky <deky@voxteneo.asia>
2015-06-26 13:20:06 +0700

    wip Index.cshtml

---
+++ Sampoerna.EMS.Website/Models/Material/MaterialIndexViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Sampoerna.EMS.Website.Models.Material
+{
+    public class MaterialIndexViewModel : BaseModel
+    {
+        public MaterialIndexViewModel()
+        {
+            Details = new List<MaterialDetail>();
+        }
+
+        public List<MaterialDetail> Details { get; set; }
+    }
+
+    public class MaterialDetail
+    {
+        public string PlantName { get; set; }
+
+        public string MaterialNumber { get; set; }
+        public string MaterialDescription { get; set; }
+
+        public string GoodTypeName { get; set; }
+        public string UomName { get; set; }
+
+        public bool PlantDeletion { get; set; }
+        public bool ClientDeletion { get; set; }
+
+    }
+}
