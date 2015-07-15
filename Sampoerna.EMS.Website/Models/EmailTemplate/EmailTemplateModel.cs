261d72c3a5898a727c597fa714083d66129f16e0
r.pratama <r.pratama@voxteneo.asia>
2015-07-15 13:33:43 +0700

    commit before merge with database-design

---
+++ Sampoerna.EMS.Website/Models/EmailTemplate/EmailTemplateModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sampoerna.EMS.Website.Models.EmailTemplate
+{
+    public class EmailTemplateModel : BaseModel
+    {
+        public int EmailTemplateId { get; set; }
+        public string EmailTemplateName { get; set; }
+
+        public string EmailTemplateBody { get; set; }
+        public string EmailTemplateSubject { get; set; }
+    }
+}
