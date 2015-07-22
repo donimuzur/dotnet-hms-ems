3806683cd602561bedd7334fca7e0a0780a8fb74
r.pratama <r.pratama@voxteneo.asia>
2015-07-22 12:55:29 +0700

    done : Email templates

---
+++ Sampoerna.EMS.Website/Models/EmailTemplate/EmailTemplateModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sampoerna.EMS.Website.Models.EmailTemplate
+{
+    public class EmailTemplateIndexModel : BaseModel {
+        public EmailTemplateIndexModel() {
+            Details = new List<EmailTemplateModel>();
+        }
+
+        public List<EmailTemplateModel> Details { get; set; }
+    }
+    public class EmailTemplateModel : BaseModel
+    {
+        public int EmailTemplateId { get; set; }
+        public string EmailTemplateName { get; set; }
+
+        public string EmailTemplateBody { get; set; }
+        public string EmailTemplateSubject { get; set; }
+    }
+}
