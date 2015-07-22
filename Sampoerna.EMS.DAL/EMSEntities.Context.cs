1d275079f60e61f62a7b15bff423f445b313581f
adnan setiawan <adnan@voxteneo.asia>
2015-07-22 13:53:46 +0700

    update EDMX, add LACK1 table

--- Sampoerna.EMS.DAL/EMSEntities.Context.cs
+++ Sampoerna.EMS.DAL/EMSEntities.Context.cs
@@ -67,7 +67,10 @@ namespace Sampoerna.EMS.BusinessObject
         public virtual DbSet<ZAIDM_EX_BRAND> ZAIDM_EX_BRAND { get; set; }
         public virtual DbSet<CARRIAGE_METHOD> CARRIAGE_METHOD { get; set; }
         public virtual DbSet<PBCK1_PROD_PLAN> PBCK1_PROD_PLAN { get; set; }
-        public virtual DbSet<WORKFLOW_HISTORY> WORKFLOW_HISTORY { get; set; }
         public virtual DbSet<CK5_MATERIAL> CK5_MATERIAL { get; set; }
+        public virtual DbSet<LACK1> LACK1 { get; set; }
+        public virtual DbSet<LACK1_DOCUMENT> LACK1_DOCUMENT { get; set; }
+        public virtual DbSet<LACK1_ITEM> LACK1_ITEM { get; set; }
+        public virtual DbSet<WORKFLOW_HISTORY> WORKFLOW_HISTORY { get; set; }
     }
 }
