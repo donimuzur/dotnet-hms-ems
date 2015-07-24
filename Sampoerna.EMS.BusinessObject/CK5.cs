f86cb4d610fca99888a5bd3422ac0895cf8083ef
Deky <deky@voxteneo.asia>
2015-07-24 17:43:05 +0700

    update table CK5 add new field

--- Sampoerna.EMS.BusinessObject/CK5.cs
+++ Sampoerna.EMS.BusinessObject/CK5.cs
@@ -76,6 +76,8 @@ namespace Sampoerna.EMS.BusinessObject
         public string DEST_PLANT_KPPBC_NAME_OFFICE { get; set; }
         public Nullable<System.DateTime> REGISTRATION_DATE { get; set; }
         public Nullable<System.DateTime> DN_DATE { get; set; }
+        public string SOURCE_PLANT_NAME { get; set; }
+        public string DEST_PLANT_NAME { get; set; }
     
         public virtual USER USER { get; set; }
         public virtual USER USER1 { get; set; }
