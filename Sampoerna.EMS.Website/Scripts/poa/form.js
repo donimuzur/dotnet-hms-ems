97f7dae0737ba24126e40cb514168d553c99ab24
adnan setiawan <adnan@voxteneo.asia>
2015-07-29 15:58:07 +0700

    update javascript for update poa sk

--- Sampoerna.EMS.Website/Scripts/poa/form.js
+++ Sampoerna.EMS.Website/Scripts/poa/form.js
@@ -14,38 +14,35 @@ $('#file-sk-container').on('click', '.add-files', (function () {
    
     $('#file-sk-container').append(' <div class="row"><div class="col-sm-9"> ' +
                                  '<input type="file" id="Detail_PoaSKFile_' + file_sk + '_" name="Detail.PoaSKFile[' + file_sk + ']" class="form-control">' +
-                             '</div><div class="col-sm-3"><button type="button" class="btn btn-danger full" onclick="removeUploadButton(' + file_sk + ', $(this))">Remove</button>' +
-                             '</div></div>');
+                             '</div><div class="col-sm-3"></div></div>');
 }));
 
 function uploadSk() {
     var poa_sk = [];
     for (var i = 0; i <= file_sk ; i++) {
-        var fileName = $('#Detail_PoaSKFile_' + i + '_').value;
-        console.log(fileName);
-        var name = fileName[0].val().replace("C:\\fakepath\\", "");
+        var fileName = $('#Detail_PoaSKFile_' + i + '_').val();
+        var name = fileName.replace("C:\\fakepath\\", "");
         poa_sk.push(name);
     }
     
+    $('#poa-files').html("");
     for (var j = 0; j < poa_sk.length; j++) {
-        $('#poa-files').append('<p>' + poa_sk[i] + '</p>');
+        $('#poa-files').append('<p id="poa_sk'+j+'">' + poa_sk[j] + '<button type="button" class="btn btn-danger full" onclick="removeUploadButton(' + j + ')">-</button></p>');
     }
 }
 
-function removeUploadButton(index, obj) {
-    if (index > 0) {
-        file_sk--;
-
+function removeUploadButton(index) {
+       file_sk--;
+        $('#poa_sk' + index).remove();
         $('#Detail_PoaSKFile_' + index + '_').remove();
-        obj.remove();
+       
         $('#file-sk-container').html("");
         $('#file-sk-container').html('<div class="row"><div class="col-sm-4"><button type="button"  class="btn btn-primary full add-files">Add Files</button></div></div>');
         for (var i = 0; i <= file_sk; i++) {
             $('#file-sk-container').append('<div class="row"><div class="col-sm-9"> ' +
                             '<input type="file" id="Detail_PoaSKFile_' + i + '_" name="Detail.PoaSKFile[' + i + ']" class="form-control">' +
-                        '</div><div class="col-sm-3"><button type="button" class="btn btn-danger full" onclick="removeUploadButton(' + i + ', $(this))">Remove</button>' +
-                        '</div></div>');
+                        '</div><div class="col-sm-3"></div></div>');
 
         }
-    }
+    
 }
