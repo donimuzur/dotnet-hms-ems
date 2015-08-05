f3be83d02a4cca8c80e1c36be2c4044416074454
a.siskayana <a.siskayana@voxteneo.asia>
2015-08-05 20:24:11 +0700

    update edmx

--- Sampoerna.EMS.BusinessObject/PRINT_HISTORY.cs
+++ Sampoerna.EMS.BusinessObject/PRINT_HISTORY.cs
@@ -20,7 +20,6 @@ namespace Sampoerna.EMS.BusinessObject
         public string FORM_NUMBER { get; set; }
         public System.DateTime PRINT_DATE { get; set; }
         public string PRINT_BY { get; set; }
-        public long PRINT_HOSTORY_ID { get; set; }
     
         public virtual USER USER { get; set; }
     }
