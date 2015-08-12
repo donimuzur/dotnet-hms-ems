5882198b262e698668d04706654c59a81bdf143d
marko <m.spasenoski@voxteneo.com.mk>
2015-08-12 11:14:42 +0200

    created create view model

---
+++ Sampoerna.EMS.Website/Models/LACK2/LACK2CreateViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sampoerna.EMS.Website.Models.LACK2
+{
+    public class LACK2CreateViewModel
+    {
+        public SelectList CompanyCodesDDL { get; set; }
+        public string SelectedCompanyCode { get; set; }
+        public SelectList NPPBKCDDL { get; set; }
+        public string SelectedNPPBKC { get; set; }
+        public SelectList SendingPlantDDL { get; set; }
+        public string SelectedPlant { get; set; }
+        public SelectList ExcisableGoodsTypeDDL { get; set; }
+        public string SelectedExGoodsType { get; set; }
+        public SelectList LACK1PeriodsDDL { get; set; }
+        public string SelectedLack1Period { get; set; }
+        public DateTime SubmissionDate { get; set; }
+    }
+}
