08b3d4d25565aff61b40626bf4ccdee2874cf65e
a.siskayana <a.siskayana@voxteneo.asia>
2015-08-17 19:26:31 +0700

    in progress

--- Sampoerna.EMS.BLL/PBCK7BLL.cs
+++ Sampoerna.EMS.BLL/PBCK7BLL.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sampoerna.EMS.BusinessObject;
 using Sampoerna.EMS.BusinessObject.DTOs;
+using Sampoerna.EMS.BusinessObject.Inputs;
 using Sampoerna.EMS.Contract;
 using Voxteneo.WebComponents.Logger;
 
@@ -13,7 +15,7 @@ namespace Sampoerna.EMS.BLL
     public class PBCK7BLL : IPBCK7BLL
     {
         private ILogger _looger;
-        //private IGenericRepository<PBCK3_PBCK7> _repository;
+        private IGenericRepository<PBCK3_PBCK7> _repository;
         private IUnitOfWork _uow;
         private IBACK1BLL _back1Bll;
 
@@ -24,11 +26,11 @@ namespace Sampoerna.EMS.BLL
             _looger = logger;
             _uow = uow;
             _back1Bll = new BACK1BLL(_uow, _looger);
-           //_repository = new _uow.GetGenericRepository<PBCK3_PBCK7>();
+            _repository = _uow.GetGenericRepository<PBCK3_PBCK7>();
            
         }
 
-        public List<Pbck7Dto> GetAllByParam(BusinessObject.Inputs.Pbck7Input input)
+        public List<Pbck7Dto> GetAllByParam(Pbck7Input input)
         {
             throw new NotImplementedException();
         }
