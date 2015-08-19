9d7d00bd606633eacfdae90084dd72f31e6e6751
adnan setiawan <adnan@voxteneo.asia>
2015-08-19 15:04:54 +0700

    adding ck5 xml writer 85% done

--- Sampoerna.EMS.XMLReader/Program.cs
+++ Sampoerna.EMS.XMLReader/Program.cs
@@ -29,20 +29,28 @@ namespace Sampoerna.EMS.XMLReader
              var ck5 = new CK5();
              ck5.REGISTRATION_NUMBER = "8100000011";
              ck5.CK5_TYPE = Enums.CK5Type.Domestic;
+             ck5.SOURCE_PLANT_ID = "ID01";
+             ck5.DEST_PLANT_ID = "ID02";
+             ck5.GI_DATE = DateTime.Now;
+             ck5.GR_DATE = DateTime.Now.AddDays(-1);
              ck5.CK5_MATERIAL = new List<CK5_MATERIAL>();
                ck5.CK5_MATERIAL.Add(new CK5_MATERIAL()
                {
                    LINE_ITEM = 1,
                    BRAND = "22.8011",
+                   CONVERTED_QTY = 1000,
+                   CONVERTED_UOM = "G"
                    
                });
                ck5.CK5_MATERIAL.Add(new CK5_MATERIAL()
                {
-                   LINE_ITEM = 2,
+                   LINE_ITEM = 10,
                    BRAND = "22.8022",
+                   CONVERTED_QTY = 1,
+                   CONVERTED_UOM = "G"
 
                });
-             rt.CreateXML(ck5);
+             rt.CreateXML(ck5, @"ck5_file.xml");
              Console.ReadLine();
         }
     }
--- Sampoerna.EMS.XMLReader/XmlCK5DataWriter.cs
+++ Sampoerna.EMS.XMLReader/XmlCK5DataWriter.cs
@@ -1,41 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using NLog.Internal;
+using NLog.Targets.Wrappers;
 using Sampoerna.EMS.BusinessObject;
+using Sampoerna.EMS.BusinessObject.DTOs;
 using Sampoerna.EMS.Contract;
+using ConfigurationManager = System.Configuration.ConfigurationManager;
+
 namespace Sampoerna.EMS.XMLReader
 {
-    public class XmlCK5DataWriter     {
-        public void CreateXML(CK5 ck5)
+    public class XmlCK5DataWriter
+    {
+
+        private string GetDateFormat(DateTime? date)
         {
-           //ZAIDM_EX_MATERIAL material1 = new ZAIDM_EX_MATERIAL();
-           //material1.STICKER_CODE = "wwewe";
-           //material1.SKEP_NP = "dsdsdsd";
-           //material1.MARKET_ID = 123;
+            var result = DateTime.MinValue;
+            if (date == null)
+                return null;
+            else
+            {
+                result =Convert.ToDateTime(date);
+            }
+            //var monthFormat = result.Month < 10 ? "0" + result.Month : result.Month.ToString();
+            //return string.Format("{0}{1}{2}", result.Year, monthFormat, result.Day);
+            return result.ToString("yyyyMMdd");
+        }
 
-           //ZAIDM_EX_MATERIAL material2 = new ZAIDM_EX_MATERIAL();
-           //material2.STICKER_CODE = "xxxxx";
-           //material2.SKEP_NP = "dsdsdsd";
-           //material2.MARKET_ID = 124;
-           //CK5 ck5 = new CK5();
-           //ck5.CK5_ID = 101;
-           ////ck5.CK5_NUMBER = "CK5OOOP0023";
-           //ck5.CK5_TYPE = Sampoerna.EMS.Core.Enums.CK5Type.Domestic;
-           //CK5_MATERIAL ck5Materialmaterial1 = new CK5_MATERIAL();
-           //ck5Materialmaterial1.CK5 = ck5;
-           //ck5Materialmaterial1.ZAIDM_EX_MATERIAL = material1;
-           //CK5_MATERIAL ck5Materialmaterial2 = new CK5_MATERIAL();
-           //ck5Materialmaterial2.CK5 = ck5;
-           //ck5Materialmaterial2.ZAIDM_EX_MATERIAL = material2;
+        private string GetLinesItem(int? line)
+        {
+            if (line == null)
+                return null;
 
-           // var listMaterial = new List<CK5_MATERIAL>();
-           // listMaterial.Add(ck5Materialmaterial1);
-           // listMaterial.Add(ck5Materialmaterial2);
+            var lineValid = Convert.ToInt32(line);
+            var result = string.Empty;
+            if (line < 10)
+                return result += "000" + lineValid;
+            if (line < 100)
+                return result += "00" + lineValid;
+            if (line < 1000)
+                return result += "0" + lineValid;
+            return null;
+        }
 
-            using (XmlWriter writer = XmlWriter.Create(@"H:\ck5_test.xml"))
+        public void CreateXML(CK5 ck5, string fileName)
+        {
+            var outboundFolder = ConfigurationManager.AppSettings["XmlOutboundPath"];
+            using (XmlWriter writer = XmlWriter.Create(Path.Combine(outboundFolder, fileName)
+                ))
             {
                 writer.WriteStartDocument();
                 writer.WriteStartElement("ZAIDM_CK5_01");
@@ -50,7 +67,12 @@ namespace Sampoerna.EMS.XMLReader
                 writer.WriteElementString("CK5_PROCS_TYP", ck5.CK5_TYPE.ToString());
                 writer.WriteElementString("SOURCE_PLANT", ck5.SOURCE_PLANT_ID);
                 writer.WriteElementString("DEST_PLANT", ck5.DEST_PLANT_ID);
-
+                writer.WriteElementString("CREATOR_ID", ck5.CREATED_BY);
+                writer.WriteElementString("STO_NUMBER", ck5.STO_SENDER_NUMBER);
+                writer.WriteElementString("STOB_NUMBER", ck5.STOB_NUMBER);
+                writer.WriteElementString("STO_REC_NUMBER", ck5.STO_RECEIVER_NUMBER);
+                writer.WriteElementString("GI_DATE", GetDateFormat(ck5.GI_DATE));
+                writer.WriteElementString("GR_DATE", GetDateFormat(ck5.GR_DATE));
 
                 
                 foreach (var item in ck5.CK5_MATERIAL)
@@ -58,8 +80,10 @@ namespace Sampoerna.EMS.XMLReader
                     writer.WriteStartElement("ZIA_CK5_ITM");
                     writer.WriteAttributeString("SEGMENT", "1");
                     writer.WriteElementString("CK5_NUMBER", ck5.REGISTRATION_NUMBER);
-                    writer.WriteElementString("ITEM_NUMBER", item.LINE_ITEM.ToString());
+                    writer.WriteElementString("ITEM_NUMBER",GetLinesItem(item.LINE_ITEM));
                     writer.WriteElementString("MATERIAL", item.BRAND);
+                    writer.WriteElementString("MENGE", item.CONVERTED_QTY.ToString());
+                    writer.WriteElementString("MEINS", item.CONVERTED_UOM.ToString());
                     //ZAIDM_CK5_ITM
                     writer.WriteEndElement();
                 }
