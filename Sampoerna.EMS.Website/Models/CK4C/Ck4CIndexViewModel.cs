68c8e921c74a9036ef2041f8a346354f2ba19dc2
a.siskayana <a.siskayana@voxteneo.asia>
2015-08-25 14:37:40 +0700

    add view model

---
+++ Sampoerna.EMS.Website/Models/CK4C/Ck4CIndexViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sampoerna.EMS.Website.Models.CK4C
+{
+    public class Ck4CIndexViewModel
+    {
+        public DateTime? ProductionDate { get; set; }
+        public string CompanyName { get; set; }
+        public string PlantId { get; set; }
+        public string PlantName { get; set; }
+
+        //selectlist
+        public SelectList CompanyNameList { get; set; }
+        public SelectList PlanIdList { get; set; }
+
+    }
+
+    public class DataIndecCk4C
+    {
+        public DateTime? ReportedOn { get; set; }
+        public string CompanyCode { get; set; }
+        public string CompanyName { get; set; }
+        public string PlantId { get; set; }
+        public string PlantName { get; set; }
+        public string FinishGoods { get; set; }
+        public string Description { get; set; }
+        public string Quality { get; set; }
+        public string Uom { get; set; }
+    }
+}
