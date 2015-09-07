933c6533187fdc0e8f3a3faa879817e854c2a90a
adnan setiawan <adnan@voxteneo.asia>
2015-09-07 16:10:08 +0700

    add prodoutput scheduler

---
+++ Sampoerna.EMS.XMLReader/XmlProdOutputDataMappers.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Sampoerna.EMS.BusinessObject;
+using Sampoerna.EMS.Contract;
+using Sampoerna.EMS.Core;
+using Sampoerna.EMS.DAL;
+using Voxteneo.WebComponents.Logger;
+namespace Sampoerna.EMS.XMLReader
+{
+    public class XmlProdOutputDataMapper : IXmlDataReader 
+    {
+        private XmlDataMapper _xmlMapper = null;
+
+        public XmlProdOutputDataMapper(string filename)
+        {
+            _xmlMapper = new XmlDataMapper(filename);
+           
+        }
+
+
+        public List<PRODUCTION> Items
+        {
+           get
+            {
+                var xmlRoot = _xmlMapper.GetElement("PRDOUTPUTDetails");
+                var xmlItems = xmlRoot.Elements("row");
+                var items = new List<PRODUCTION>();
+                foreach (var xElement in xmlItems)
+                {
+                    try
+                    {
+                        var item = new PRODUCTION();
+                        item.WERKS = _xmlMapper.GetElementValue(xElement.Element("Plnt")); ;
+                        item.FA_CODE = _xmlMapper.GetElementValue(xElement.Element("Material"));
+                        item.BRAND_DESC = _xmlMapper.GetElementValue(xElement.Element("MaterialDescriptionMaterialDescription"));
+                        item.PRODUCTION_DATE = Convert.ToDateTime(_xmlMapper.GetDateDotSeparator(_xmlMapper.GetElementValue(xElement.Element("ProdDate"))));
+                       
+                        //item.BUN = _xmlMapper.GetElementValue(xElement.Element("BUn"));
+                        items.Add(item);
+                    }
+                    catch (Exception ex)
+                    {
+                        _xmlMapper.Errors.Add(ex.Message);
+                        continue;
+                        
+                    }
+                   
+
+                }
+                return items;
+            }
+             
+        }
+
+
+        public string InsertToDatabase()
+        {
+            
+            
+            return _xmlMapper.InsertToDatabase<PRODUCTION>(Items);
+       
+        }
+
+        public List<string> GetErrorList()
+        {
+            return _xmlMapper.Errors;
+        }
+      
+
+
+
+    }
+}
