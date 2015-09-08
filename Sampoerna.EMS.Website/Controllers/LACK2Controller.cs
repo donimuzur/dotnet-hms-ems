1f6556a38d743c135f1095968535045ad0fd64b1
adnan setiawan <adnan@voxteneo.asia>
2015-09-08 10:40:47 +0700

    fixing bug when edit lack2

--- Sampoerna.EMS.Website/Controllers/LACK2Controller.cs
+++ Sampoerna.EMS.Website/Controllers/LACK2Controller.cs
@@ -285,10 +285,19 @@ namespace Sampoerna.EMS.Website.Controllers
             {
                 return RedirectToAction("Submit", new {id = model.Lack2Model.Lack2Id});
             }
-            Lack2Dto item = new Lack2Dto();
-            
-            item = AutoMapper.Mapper.Map<Lack2Dto>(model.Lack2Model);
 
+              var item = AutoMapper.Mapper.Map<Lack2Dto>(model.Lack2Model);
+              var exItems = new Lack2ItemDto[item.Items.Count];
+              item.Items.CopyTo(exItems);
+              item.Items = new List<Lack2ItemDto>();
+              foreach (var items in exItems)
+              {
+                    if (items.Id == 0 )
+                    {
+                        item.Items.Add(items);
+                    }
+              }
+           
             var plant = _plantBll.GetT001ById(model.Lack2Model.LevelPlantId);
             var company = _companyBll.GetById(model.Lack2Model.Burks);
             var goods = _exGroupBll.GetById(model.Lack2Model.ExGoodTyp);
