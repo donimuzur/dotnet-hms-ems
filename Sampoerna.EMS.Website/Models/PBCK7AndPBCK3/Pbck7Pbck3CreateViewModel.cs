d6e47ff134c0beb65d0f27cc8bb60f6a056c9c84
adnan setiawan <adnan@voxteneo.asia>
2015-09-11 16:14:11 +0700

    add enums DocumentTypeList

--- Sampoerna.EMS.Website/Models/PBCK7AndPBCK3/Pbck7Pbck3CreateViewModel.cs
+++ Sampoerna.EMS.Website/Models/PBCK7AndPBCK3/Pbck7Pbck3CreateViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Sampoerna.EMS.BusinessObject.DTOs;
+using Sampoerna.EMS.Core;
 
 namespace Sampoerna.EMS.Website.Models.PBCK7AndPBCK3
 {
@@ -13,7 +14,7 @@ namespace Sampoerna.EMS.Website.Models.PBCK7AndPBCK3
         public DateTime Pbck7Date { get; set; }
         public string Pbck3Number { get; set; }
         public DateTime? Pbck3Date { get; set; }
-        public string DocumentType { get; set; }
+        public Enums.DocumentTypePbck7AndPbck3 DocumentType { get; set; }
         public DateTime? ExecDateFrom { get; set; }
         public DateTime? ExecDateTo { get; set; }
         public string NppbkcId { get; set; }
@@ -24,7 +25,7 @@ namespace Sampoerna.EMS.Website.Models.PBCK7AndPBCK3
         public SelectList NppbkIdList { get; set; }
         public SelectList PlantList { get; set; }
 
-        public SelectList DocumentTypeList { get; set; }
+        public Enums.DocumentTypePbck7AndPbck3 DocumentTypeList { get; set; }
 
         public List<Pbck7ItemUpload> UploadItems { get; set; }
 
