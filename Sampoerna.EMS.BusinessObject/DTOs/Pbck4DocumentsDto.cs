de55b52f206c0c2342cbe3aee4dbc8039925fac3
Deky <deky@voxteneo.asia>
2015-09-15 10:01:15 +0700

    wip

---
+++ Sampoerna.EMS.BusinessObject/DTOs/Pbck4DocumentsDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sampoerna.EMS.BusinessObject.DTOs
+{
+   public class Pbck4DocumentsDto
+    {
+        public long PBCK4_DOCUMENT_ID { get; set; }
+        public int DOC_TYPE { get; set; }
+        public string FILE_NAME { get; set; }
+        public string FILE_PATH { get; set; }
+        public int PBCK4_ID { get; set; }
+    }
+}
