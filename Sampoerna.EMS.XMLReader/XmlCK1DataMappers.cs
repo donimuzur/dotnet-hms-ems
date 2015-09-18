aac719a9886814c1c4851d9609896cd9e7d56ab8
adnan setiawan <adnan@voxteneo.asia>
2015-09-18 15:29:21 +0700

    update c1 scheduler

--- Sampoerna.EMS.XMLReader/XmlCK1DataMappers.cs
+++ Sampoerna.EMS.XMLReader/XmlCK1DataMappers.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Xml.Linq;
 using Sampoerna.EMS.BusinessObject;
 using Sampoerna.EMS.Contract;
 using Sampoerna.EMS.Core;
@@ -25,10 +26,10 @@ namespace Sampoerna.EMS.XMLReader
             get
             {
                 var xmlRoot = _xmlMapper.GetElement("IDOC");
-                var xmlRoot2 = xmlRoot.Element("Z1A_EKKO_SSPCP");
-                var xmlItems = xmlRoot2.Elements("Z1A_EKPO_SSPCP");
+                var xmlRoot2 = xmlRoot.Elements("Z1A_EKKO_SSPCP");
+
                 var items = new List<CK1>();
-                foreach (var xElement in xmlItems)
+                foreach (var xElement in xmlRoot2)
                 {
                     try
                     {
@@ -36,13 +37,30 @@ namespace Sampoerna.EMS.XMLReader
                         item.CK1_NUMBER = _xmlMapper.GetElementValue(xElement.Element("EBELN"));
 
                         item.CK1_DATE = Convert.ToDateTime(_xmlMapper.GetDate(_xmlMapper.GetElementValue(xElement.Element("AEDAT"))));
-                        //var detail = new CK1_ITEM();
-                        //detail.FA_CODE = _xmlMapper.GetElementValue(xElement.Element("FA_CODE"));
-                        //detail.MATERIAL_ID = _xmlMapper.GetElementValue(xElement.Element("MATNR"));
-                        //item.CK1_ITEM = new List<CK1_ITEM>();
-                        //item.CK1_ITEM.Add(detail);
                         item.PLANT_ID = _xmlMapper.GetElementValue(xElement.Element("WERKS"));
                         item.CREATED_BY = Constans.PI;
+                        item.COMPANY_ID = _xmlMapper.GetElementValue(xElement.Element("BUKRS"));
+                        item.NPPBKC_ID = _xmlMapper.GetElementValue(xElement.Element("NPPBKC_ID"));
+                        item.VENDOR_ID = _xmlMapper.GetElementValue(xElement.Element("LIFNR"));
+                        item.ORDER_DATE = _xmlMapper.GetDate(_xmlMapper.GetElementValue(xElement.Element("ORDER_DATE")));
+                        var company = _xmlMapper.uow.GetGenericRepository<T001>().GetByID(item.COMPANY_ID);
+                        if (company != null)
+                        {
+
+                            item.COMPANY_NAME = company.BUTXT;
+                        }
+                        var xmlItems = xElement.Elements("Z1A_EKPO_SSPCP");
+                        item.CK1_ITEM = new List<CK1_ITEM>();
+                        foreach (var xElementItem in xmlItems)
+                        {
+                            var detail = new CK1_ITEM();
+                            detail.FA_CODE = _xmlMapper.GetElementValue(xElementItem.Element("FA_CODE"));
+                            detail.MATERIAL_ID = _xmlMapper.GetElementValue(xElementItem.Element("MATNR"));
+                            detail.WERKS = _xmlMapper.GetElementValue(xElementItem.Element("WERKS"));
+                            detail.MENGE = Convert.ToDecimal(_xmlMapper.GetElementValue(xElementItem.Element("MENGE")));
+                            detail.UOM = _xmlMapper.GetElementValue(xElementItem.Element("MEINS"));
+                            item.CK1_ITEM.Add(detail);
+                        }
                         var existingData = GetCk1(item.CK1_NUMBER);
 
                         if (existingData != null)
