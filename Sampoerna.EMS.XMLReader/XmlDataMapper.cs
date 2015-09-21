f51e74e5a2a574757b8c1efb40ef619b1b2eb126
r.pratama <r.pratama@voxteneo.asia>
2015-09-21 15:20:44 +0700

    refactor

--- Sampoerna.EMS.XMLReader/XmlDataMapper.cs
+++ Sampoerna.EMS.XMLReader/XmlDataMapper.cs
@@ -76,16 +76,12 @@ namespace Sampoerna.EMS.XMLReader
                 foreach (var item in existingData)
                 {
                     if (item is LFA1)
-                    {
                         continue;
-                    }
-                    else if (item is ZAIDM_EX_BRAND || item is ZAIDM_EX_MATERIAL)
-                    {
-                        var isFromSap = item.GetType().GetProperty("IS_FROM_SAP") != null && (bool)item.GetType().GetProperty("IS_FROM_SAP").GetValue(item);
-                        if(isFromSap)
-                            continue;
-                    }
-                    
+
+                    var isFromSap = item.GetType().GetProperty("IS_FROM_SAP") != null && (bool)item.GetType().GetProperty("IS_FROM_SAP").GetValue(item);
+                    if (!isFromSap)
+                        continue;
+
 
                     var is_deleted = item.GetType().GetProperty("IS_DELETED");
                     if (is_deleted != null)
