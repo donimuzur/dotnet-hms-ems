777bd49bb1423ee175cf19515d14ef5789bed40d
a.siskayana <a.siskayana@voxteneo.asia>
2015-09-23 10:57:10 +0700

    work in progress in fixing bug upload waste and production

--- Sampoerna.EMS.BLL/ProductionBLL.cs
+++ Sampoerna.EMS.BLL/ProductionBLL.cs
@@ -92,7 +92,7 @@ namespace Sampoerna.EMS.BLL
 
             var originDto = Mapper.Map<ProductionDto>(origin);
 
-            SetChange(originDto, productionDto, userId);
+            SetChange(originDto, dbProduction, userId);
             dbProduction.CREATED_DATE = DateTime.Now;
 
             if (dbProduction.UOM == "KG")
@@ -208,21 +208,21 @@ namespace Sampoerna.EMS.BLL
             var dbUpload = Mapper.Map<PRODUCTION>(uploadItems);
 
             _repository.InsertOrUpdate(dbUpload);
-            dbUpload.CREATED_DATE = DateTime.Now;
+           
             _uow.SaveChanges();
         }
 
-        private void SetChange(ProductionDto origin, ProductionDto data, string userId)
+        private void SetChange(ProductionDto origin, PRODUCTION data, string userId)
         {
             var changeData = new Dictionary<string, bool>();
-            changeData.Add("COMPANY_CODE", origin.CompanyCode == data.CompanyCode);
-            changeData.Add("WERKS", origin.PlantWerks == data.PlantWerks);
-            changeData.Add("FA_CODE", origin.FaCode == data.FaCode);
-            changeData.Add("PRODUCTION_DATE", origin.ProductionDate == data.ProductionDate);
-            changeData.Add("BRAND_DESC", origin.BrandDescription == data.BrandDescription);
-            changeData.Add("QTY_PACKED", origin.QtyPacked == data.QtyPacked);
-            changeData.Add("QTY_UNPACKED", origin.QtyUnpacked == data.QtyUnpacked);
-            changeData.Add("UOM", origin.Uom == data.Uom);
+            changeData.Add("COMPANY_CODE", origin.CompanyCode == data.COMPANY_CODE);
+            changeData.Add("WERKS", origin.PlantWerks == data.WERKS);
+            changeData.Add("FA_CODE", origin.FaCode == data.FA_CODE);
+            changeData.Add("PRODUCTION_DATE", origin.ProductionDate == data.PRODUCTION_DATE);
+            changeData.Add("BRAND_DESC", origin.BrandDescription == data.BRAND_DESC);
+            changeData.Add("QTY_PACKED", origin.QtyPacked == data.QTY_PACKED);
+            changeData.Add("QTY_UNPACKED", origin.QtyUnpacked == data.QTY_UNPACKED);
+            changeData.Add("UOM", origin.Uom == data.UOM);
 
             foreach (var listChange in changeData)
             {
@@ -231,7 +231,7 @@ namespace Sampoerna.EMS.BLL
                     var changes = new CHANGES_HISTORY
                     {
                         FORM_TYPE_ID = Core.Enums.MenuList.CK4C,
-                        FORM_ID = data.CompanyCode+"_" + data.PlantWerks+"_" + data.FaCode+"_" + data.ProductionDate.ToString("ddMMMyyyy"),
+                        FORM_ID = data.COMPANY_CODE + "_" + data.WERKS + "_" + data.FA_CODE + "_" + data.PRODUCTION_DATE.ToString("ddMMMyyyy"),
                         FIELD_NAME = listChange.Key,
                         MODIFIED_BY = userId,
                         MODIFIED_DATE = DateTime.Now
@@ -241,35 +241,35 @@ namespace Sampoerna.EMS.BLL
                     {
                         case "COMPANY_CODE":
                             changes.OLD_VALUE = origin.CompanyCode;
-                            changes.NEW_VALUE = data.CompanyCode;
+                            changes.NEW_VALUE = data.COMPANY_CODE;
                             break;
                         case "WERKS":
                             changes.OLD_VALUE = origin.PlantWerks;
-                            changes.NEW_VALUE = data.PlantWerks;
+                            changes.NEW_VALUE = data.WERKS;
                             break;
                         case "FA_CODE":
                             changes.OLD_VALUE = origin.FaCode;
-                            changes.NEW_VALUE = data.FaCode;
+                            changes.NEW_VALUE = data.FA_CODE;
                             break;
                         case "PRODUCTION_DATE":
                             changes.OLD_VALUE = origin.ProductionDate.ToString();
-                            changes.NEW_VALUE = data.ProductionDate.ToString();
+                            changes.NEW_VALUE = data.PRODUCTION_DATE.ToString();
                             break;
                         case "BRAND_DESC":
                             changes.OLD_VALUE = origin.BrandDescription;
-                            changes.NEW_VALUE = data.BrandDescription;
+                            changes.NEW_VALUE = data.BRAND_DESC;
                             break;
                         case "QTY_PACKED":
                             changes.OLD_VALUE = origin.QtyPacked.ToString();
-                            changes.NEW_VALUE = data.QtyPacked.ToString();
+                            changes.NEW_VALUE = data.QTY_PACKED.ToString();
                             break;
                         case "QTY_UNPACKED":
                             changes.OLD_VALUE = origin.QtyUnpacked.ToString();
-                            changes.NEW_VALUE = data.QtyUnpacked.ToString();
+                            changes.NEW_VALUE = data.QTY_UNPACKED.ToString();
                             break;
                         case "UOM":
                             changes.OLD_VALUE = origin.Uom;
-                            changes.NEW_VALUE = data.Uom;
+                            changes.NEW_VALUE = data.UOM;
                             break;
                         default: break;
                     }
--- Sampoerna.EMS.Website/Controllers/WasteController.cs
+++ Sampoerna.EMS.Website/Controllers/WasteController.cs
@@ -360,6 +360,8 @@ namespace Sampoerna.EMS.Website.Controllers
                     item.CompanyName = company.BUTXT;
                     item.PlantName = plant.NAME1;
 
+                    item.CreatedDate = DateTime.Now;
+
                     _wasteBll.SaveUpload(item);
                     AddMessageInfo(Constans.SubmitMessage.Saved, Enums.MessageInfoType.Success
                        );
@@ -386,17 +388,18 @@ namespace Sampoerna.EMS.Website.Controllers
                 {
                     var item = new WasteUploadItems();
 
+                  
                     item.CompanyCode = dataRow[0];
                     item.PlantWerks = dataRow[1];
                     item.FaCode = dataRow[2];
                     item.BrandDescription = dataRow[3];
-                    item.PackerRejectStickQty = Convert.ToDecimal(dataRow[4]);
-                    item.MarkerRejectStickQty = Convert.ToDecimal(dataRow[5]);
+                    item.MarkerRejectStickQty = Convert.ToDecimal(dataRow[4]);
+                    item.PackerRejectStickQty = Convert.ToDecimal(dataRow[5]);
                     item.DustWasteGramQty = Convert.ToDecimal(dataRow[6]);
                     item.FloorWasteGramQty = Convert.ToDecimal(dataRow[7]);
                     item.DustWasteStickQty = Convert.ToDecimal(dataRow[8]);
                     item.FloorWasteStickQty = Convert.ToDecimal(dataRow[9]);
-                    item.WasteProductionDate = DateTime.FromOADate(Convert.ToDouble(data.DataRows[0][10])).ToString("dd MMM yyyy");
+                    item.WasteProductionDate = DateTime.FromOADate(Convert.ToDouble(dataRow[10])).ToString("dd MMM yyyy");
 
                     {
                         model.Add(item);
