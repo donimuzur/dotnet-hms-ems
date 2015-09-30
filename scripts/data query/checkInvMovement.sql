beaa5d14cb242a10b3ca18ea43071310b4e43161
r.pratama <r.pratama@voxteneo.asia>
2015-09-30 15:11:10 +0700

    fixup! parent

--- scripts/data query/checkInvMovement.sql
+++
@@ -1,9 +0,0 @@
-DECLARE @periodMonth int = 9
-DECLARE @periodYear int = 2015
-DECLARE @destPlantId nvarchar(5) = 'ID01'
-
-SELECT * FROM INVENTORY_MOVEMENT
-WHERE MVT IN('261', '262') AND YEAR(POSTING_DATE) = @periodYear AND MONTH(POSTING_DATE) = @periodMonth
-		AND PLANT_ID = @destPlantId
-
-SELECT * FROM LACK1
