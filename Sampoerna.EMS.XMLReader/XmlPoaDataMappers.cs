2070e3273c39529eacab64e2a27ac0fc425cf0c7
r.pratama <r.pratama@voxteneo.asia>
2015-10-05 14:05:29 +0700

    revert poa

--- Sampoerna.EMS.XMLReader/XmlPoaDataMappers.cs
+++ Sampoerna.EMS.XMLReader/XmlPoaDataMappers.cs
@@ -17,12 +17,62 @@ namespace Sampoerna.EMS.XMLReader
             ErroList = new List<string>();
         }
 
-        
-      
-      
+        public List<POA> Items
+        {
+            get
+            {
+                var xmlItems = _xmlMapper.GetElements("ITEM");
+                var items = new List<POA>();
+                foreach (var xElement in xmlItems)
+                {
+                    var item = new POA();
+                    var poaCodeXml = xElement.Element("POA_ID").Value;
+                    var exisitingPoa = GetExPoa(poaCodeXml);
+                    var podDateXml = Convert.ToDateTime(xElement.Element("MODIFIED_DATE").Value);
+                    item.POA_ID = poaCodeXml;
+                    item.ID_CARD = xElement.Element("POA_ID_CARD").Value;
+                    item.PRINTED_NAME = xElement.Element("POA_PRINTED_NAME").Value;
+                    item.POA_PHONE = xElement.Element("POA_PHONE").Value;
+                    item.POA_ADDRESS = xElement.Element("POA_ADDRESS").Value;
+                    item.CREATED_DATE = DateTime.Now;
+                    item.IS_ACTIVE = true;
+                    if (exisitingPoa != null)
+                    {
+                        if (podDateXml > exisitingPoa.CREATED_DATE)
+                        {
+                            items.Add(item);
+                        }
+                        else
+                        {
+                            continue;
+
+                        }
+                    }
+                    else
+                    {
+                        items.Add(item);
+                    }
+
+                }
+                return items;
+            }
+
+        }
+
+
         public string InsertToDatabase()
         {
-          return _xmlMapper.MoveFile();
+            return _xmlMapper.InsertToDatabase<POA>(Items);
+
+        }
+
+        public POA GetExPoa(string PoaCode)
+        {
+            var exisitingPoa = _xmlMapper.uow.GetGenericRepository<POA>()
+                            .Get(p => p.POA_ID == PoaCode)
+                            .OrderByDescending(p => p.CREATED_DATE)
+                            .FirstOrDefault();
+            return exisitingPoa;
         }
 
         public List<string> GetErrorList()
