f4b7bb84cae4672c33b32866750fb441e780fd5e
r.pratama <r.pratama@voxteneo.asia>
2015-10-15 09:10:56 +0700

    domestic alcohol wip

---
+++ Sampoerna.EMS.Website/Models/CK5/CK5ExternalSupplierModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sampoerna.EMS.Website.Models.CK5
+{
+    public class CK5ExternalSupplierModel
+    {
+
+        public string SupplierName { get; set; }
+        public string SuppierNpwp { get; set; }
+        public string NPPBCK_ID { get; set; }
+        //public string CompanyName { get; set; }
+        public string SupplierAddress { get; set; }
+        //public string KppBcName { get; set; }
+        public string PlantName { get; set; }
+        //public string CompanyCode { get; set; }
+        public string KppbcCity { get; set; }
+        public string KppbcNo { get; set; }
+
+        public int? Pbck1Id { get; set; }
+        public string Pbck1Number { get; set; }
+        public string Pbck1QtyApproved { get; set; }
+        public string Ck5TotalExciseable { get; set; }
+        public string RemainQuota { get; set; }
+        public string Pbck1DecreeDate { get; set; }
+
+        public string PbckUom { get; set; }
+
+        public List<Ck5ListPbck1Completed> PbckList { get; set; }
+
+        public SelectList CorrespondingPlantList { get; set; }
+    }
+}
