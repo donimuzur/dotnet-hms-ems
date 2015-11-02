319609909653979c3d7294d844022d356d46b306
irman <irman@voxteneo.asia>
2015-11-02 13:46:04 +0700

    wip

---
+++ Sampoerna.EMS.BLL/Services/ZaapShiftRptService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Sampoerna.EMS.BusinessObject;
+using Sampoerna.EMS.BusinessObject.Inputs;
+using Sampoerna.EMS.Contract;
+using Sampoerna.EMS.Contract.Services;
+using Sampoerna.EMS.Core.Exceptions;
+using Sampoerna.EMS.Utils;
+using Voxteneo.WebComponents.Logger;
+using System.Linq;
+
+namespace Sampoerna.EMS.BLL.Services
+{
+    public class ZaapShiftRptService : IZaapShiftRptService
+    {
+        private IGenericRepository<ZAAP_SHIFT_RPT> _repository;
+        private ILogger _logger;
+        private IUnitOfWork _uow;
+
+        public ZaapShiftRptService(IUnitOfWork uow, ILogger logger)
+        {
+            _logger = logger;
+            _uow = uow;
+            _repository = _uow.GetGenericRepository<ZAAP_SHIFT_RPT>();
+        }
+
+        public List<ZAAP_SHIFT_RPT> GetForLack1ByParam(ZaapShiftRptGetForLack1ByParamInput input)
+        {
+            Expression<Func<ZAAP_SHIFT_RPT, bool>> queryFilter = c => c.COMPANY_CODE == input.CompanyCode && c.WERKS == input.Werks 
+                && c.PRODUCTION_DATE.Year == input.PeriodYear && c.PRODUCTION_DATE.Month == input.PeriodMonth;
+
+            if (input.FaCodeList != null && input.FaCodeList.Count > 0)
+            {
+                queryFilter = queryFilter.And(c => input.FaCodeList.Contains(c.FA_CODE));
+            }
+
+            var dbData = _repository.Get(queryFilter);
+
+            if (dbData == null)
+            {
+                throw new BLLException(ExceptionCodes.BLLExceptions.DataNotFound);
+            }
+
+            return dbData.ToList();
+
+        }
+    }
+}
