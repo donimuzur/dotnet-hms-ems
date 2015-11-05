6e402baed6b65167672701b438bf4955e7f5409e
Yuniar Prasetyo <yuniar@voxteneo.asia>
2015-11-05 19:41:00 +0700

    change number of status in dashboard

---
+++ Sampoerna.EMS.Website/Scripts/Dashboard.js
@@ -0,0 +1,13 @@
+﻿function ChangeNumber(value) {
+    var realValue = value;
+
+    if (value > 9999) {
+        realValue = (value / 1000);
+    }
+
+    if (value > 9999) {
+        realValue = realValue + "K";
+    }
+
+    return realValue;
+}
