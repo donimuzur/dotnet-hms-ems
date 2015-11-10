60d82dd0b176c607eb1df65dee088f0d68aec7cd
irman <irman@voxteneo.asia>
2015-11-10 16:44:37 +0700

    fix double document number on generate document number

--- Sampoerna.EMS.BLL/DocumentSequenceNumberBLL.cs
+++ Sampoerna.EMS.BLL/DocumentSequenceNumberBLL.cs
@@ -15,6 +15,7 @@ namespace Sampoerna.EMS.BLL
         private ILogger _logger;
         private IUnitOfWork _uow;
         private IGenericRepository<DOC_NUMBER_SEQ> _repository;
+        private IGenericRepository<WORKFLOW_HISTORY> _workflowHitoryRepository;
         //private IGenericRepository<ZAIDM_EX_NPPBKC> _nppbkcRepository;
         private IGenericRepository<T001K> _t001KReporRepository;
 
@@ -25,6 +26,7 @@ namespace Sampoerna.EMS.BLL
             _uow = uow;
             _repository = _uow.GetGenericRepository<DOC_NUMBER_SEQ>();
             _t001KReporRepository = _uow.GetGenericRepository<T001K>();
+            _workflowHitoryRepository = _uow.GetGenericRepository<WORKFLOW_HISTORY>();
         }
 
         //public string GenerateNumber(GenerateDocNumberInput input)
@@ -99,6 +101,35 @@ namespace Sampoerna.EMS.BLL
         /// <returns></returns>
         public string GenerateNumberByFormType(Enums.FormType formType)
         {
+            while (true)
+            {
+                DOC_NUMBER_SEQ docSeqNumberToInsertOrUpdate;
+
+                var docNumber = GetGenerateNumberByFormType(formType, out docSeqNumberToInsertOrUpdate);
+                var isAlreadyExists = IsDocumentNumberAlreadyExists(docNumber);
+
+                if (isAlreadyExists)
+                    continue;
+
+                if (docSeqNumberToInsertOrUpdate.DOC_NUMBER_SEQ_ID > 0)
+                {
+                    //update
+                    _repository.Update(docSeqNumberToInsertOrUpdate);
+                }
+                else
+                {
+                    //insert new record
+                    _repository.Insert(docSeqNumberToInsertOrUpdate);
+                }
+
+                _uow.SaveChanges();
+
+                return docNumber;
+            }
+        }
+
+        private string GetGenerateNumberByFormType(Enums.FormType formType, out DOC_NUMBER_SEQ docSeqNumberToInsertOrUpdate)
+        {
             var lastSeqData = _repository.Get(c => c.FORM_TYPE_ID == formType).FirstOrDefault();
 
             if (lastSeqData == null)
@@ -111,23 +142,48 @@ namespace Sampoerna.EMS.BLL
                     FORM_TYPE_ID = formType,
                     DOC_NUMBER_SEQ_LAST = 1
                 };
-                _repository.Insert(lastSeqData);
             }
             else
             {
                 lastSeqData.DOC_NUMBER_SEQ_LAST += 1;
-                _repository.Update(lastSeqData);
             }
 
+            docSeqNumberToInsertOrUpdate = lastSeqData;
+
             var docNumber = lastSeqData.DOC_NUMBER_SEQ_LAST.ToString("0000000000");
-
-            _uow.SaveChanges();
-
             return docNumber;
-
         }
 
         public string GenerateNumber(GenerateDocNumberInput input)
+        {
+            while (true)
+            {
+                DOC_NUMBER_SEQ docSeqNumberToInsertOrUpdate;
+
+                var docNumber = GetGenerateNumber(input, out docSeqNumberToInsertOrUpdate);
+                var isAlreadyExists = IsDocumentNumberAlreadyExists(docNumber);
+
+                if (isAlreadyExists)
+                    continue;
+
+                if (docSeqNumberToInsertOrUpdate.DOC_NUMBER_SEQ_ID > 0)
+                {
+                    //update
+                    _repository.Update(docSeqNumberToInsertOrUpdate);
+                }
+                else
+                {
+                    //insert new record
+                    _repository.Insert(docSeqNumberToInsertOrUpdate);
+                }
+
+                _uow.SaveChanges();
+
+                return docNumber;
+            }
+        }
+
+        private string GetGenerateNumber(GenerateDocNumberInput input, out DOC_NUMBER_SEQ docSeqNumberToInsertOrUpdate)
         {
             var lastSeqData = _repository.Get(c => c.FORM_TYPE_ID != Enums.FormType.CK5).FirstOrDefault();
 
@@ -140,12 +196,10 @@ namespace Sampoerna.EMS.BLL
                     MONTH = input.Month,
                     DOC_NUMBER_SEQ_LAST = 1
                 };
-                _repository.Insert(lastSeqData);
             }
             else
             {
                 lastSeqData.DOC_NUMBER_SEQ_LAST += 1;
-                _repository.Update(lastSeqData);
             }
 
             //var docNumber = lastSeqData.DOC_NUMBER_SEQ_LAST.ToString();//log : 2015-10-12
@@ -156,7 +210,7 @@ namespace Sampoerna.EMS.BLL
                 var t001Data =
                     _t001KReporRepository.Get(
                         c =>
-                            (c.T001W.NPPBKC_ID == input.NppbkcId || c.T001W.NPPBKC_IMPORT_ID == input.NppbkcId)  && c.T001W.IS_MAIN_PLANT.HasValue &&
+                            (c.T001W.NPPBKC_ID == input.NppbkcId || c.T001W.NPPBKC_IMPORT_ID == input.NppbkcId) && c.T001W.IS_MAIN_PLANT.HasValue &&
                             c.T001W.IS_MAIN_PLANT.Value, null, "T001, T001W, T001W.ZAIDM_EX_NPPBKC").FirstOrDefault();
 
                 if (t001Data == null || t001Data.T001 == null)
@@ -183,18 +237,22 @@ namespace Sampoerna.EMS.BLL
                 docNumber = docNumber + "/" + t001Data.T001.BUTXT_ALIAS + "/" + t001Data.T001W.ZAIDM_EX_NPPBKC.CITY_ALIAS
                             + "/" + MonthHelper.ConvertToRomansNumeral(input.Month) + "/" + input.Year;
 
-
-
             }
 
-            _uow.SaveChanges();
+            docSeqNumberToInsertOrUpdate = lastSeqData;
 
             return docNumber;
         }
-        
+
         public List<DOC_NUMBER_SEQ> GetDocumentSequenceList()
         {
             return _repository. Get(null, null, "MONTH1").ToList();
+        }
+
+        private bool IsDocumentNumberAlreadyExists(string documentNumber)
+        {
+            var dbCheck = _workflowHitoryRepository.Get(c => c.FORM_NUMBER == documentNumber).FirstOrDefault();
+            return dbCheck != null;
         }
 
     }
