86775a7f178a637d31cf2d1248990846e87f96b5
radit <radit@voxteneo.co.id>
2015-12-01 17:37:33 +0700

    update script initial material master

--- scripts/Initial Data/initial data material master.sql
+++ scripts/Initial Data/initial data material master.sql
@@ -9,7 +9,7 @@ DECLARE @uom_id varchar(10), @umren decimal(18,4)
 , @uomexist int,@formtypematerialmaster int,@modifieduser varchar(50)
 ,@goodtyp varchar(5),@tempgoodtyp varchar(5);
 
-set @goodtyp = '02'; -- good type code for TIS
+--set @goodtyp = '02'; -- good type code for TIS
 
 set @uom_id = 'G';
 set @umren = 0.0010;
@@ -29,11 +29,11 @@ BEGIN
 	select @uomexist = count(*) from MATERIAL_UOM where WERKS = @werks and STICKER_CODE = @sticker_code and MEINH = @uom_id ;
 	select @tempgoodtyp = EXC_GOOD_TYP from ZAIDM_EX_MATERIAL where WERKS = @werks and STICKER_CODE = @sticker_code;
 
-	if @uomexist = 0 and @tempgoodtyp = @goodtyp
+	if @uomexist = 0 and @tempgoodtyp in ('02','03') -- '02' for TIS and '03' for TIS reject
 	begin
 		insert into MATERIAL_UOM(STICKER_CODE,WERKS,MEINH,UMREN) values(@sticker_code,@werks,@uom_id,@umren);
 		insert into CHANGES_HISTORY(FORM_TYPE_ID,FORM_ID,FIELD_NAME,OLD_VALUE,NEW_VALUE,MODIFIED_DATE,MODIFIED_BY)
-		VALUES(@formtypematerialmaster,CONCAT(@sticker_code,@werks),'CONVERTION ADDED','',CONCAT(@uom_id,' - ',@umren),CURRENT_TIMESTAMP,@modifieduser);
+		VALUES(@formtypematerialmaster,@sticker_code+@werks,'CONVERTION ADDED','',@uom_id+' - '+CONVERT(varchar(10),@umren,0),CURRENT_TIMESTAMP,@modifieduser);
 	end
 	FETCH NEXT FROM material_cursor 
 	INTO @werks, @sticker_code;
