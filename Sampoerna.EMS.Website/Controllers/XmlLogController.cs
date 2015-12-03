bb40978d56edfa57139134e1f138df056db88391
Deky <deky@voxteneo.asia>
2015-12-03 09:53:53 +0700

    wip

---
+++ Sampoerna.EMS.Website/Controllers/XmlLogController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sampoerna.EMS.Website.Controllers
+{
+    public class XmlLogController : Controller
+    {
+        //
+        // GET: /XmlLog/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ Sampoerna.EMS.Website/Models/XmlLog/XmlLogFormViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Sampoerna.EMS.Website.Models.XmlLog
+{
+    public class XmlLogFormViewModel : BaseModel
+    {
+        public long XmlLogId { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
