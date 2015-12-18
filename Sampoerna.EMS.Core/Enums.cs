9db350440d8bba868875274d8d36fd5bdba2bac4
Deky <deky@voxteneo.asia>
2015-12-18 15:35:14 +0700

    add new table XML_LOGS and XML_LOGS_DETAILS.cs

--- Sampoerna.EMS.Core/Enums.cs
+++ Sampoerna.EMS.Core/Enums.cs
@@ -657,5 +657,12 @@ namespace Sampoerna.EMS.Core
             WasteApprover = 4
         }
 
+        public enum XmlLogStatus
+        {
+            [Description("Error")]
+            Approved = 1,
+            [Description("Re-Run")]
+            ReRun = 2
+        }
     }
 }
