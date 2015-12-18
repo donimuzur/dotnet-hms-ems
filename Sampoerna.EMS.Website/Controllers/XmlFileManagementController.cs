29aea89b6b2dee31dba59177c9ec7f7c0e36b385
Deky <deky@voxteneo.asia>
2015-12-18 14:33:32 +0700

    wip

---
+++ Sampoerna.EMS.Website/Controllers/XmlFileManagementController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sampoerna.EMS.Website.Controllers
+{
+    public class XmlFileManagementController : Controller
+    {
+        //
+        // GET: /XmlFileManagement/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
