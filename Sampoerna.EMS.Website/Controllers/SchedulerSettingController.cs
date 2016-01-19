141674439b487153c6f4d5cddfbd1c20fbc1114d
radit <radit@voxteneo.co.id>
2016-01-19 15:32:54 +0700

    wip

---
+++ Sampoerna.EMS.Website/Controllers/SchedulerSettingController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sampoerna.EMS.Contract;
+using Sampoerna.EMS.Core;
+
+namespace Sampoerna.EMS.Website.Controllers
+{
+    public class SchedulerSettingController : BaseController
+    {
+        //
+        // GET: /SchedulerSetting/
+        public SchedulerSettingController(IPageBLL pageBll) : base(pageBll, Enums.MenuList.SchedulerSettings)
+        {
+
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
