059ee3b3006a1a7455d3dc3e52b18c31aa08c543
Yuniar Prasetyo <tyo@voxteneo.co.id>
2016-01-20 17:52:10 +0700

    adjust lack1 for usage 201
    (cherry picked from commit 3b5cd6a4000772e2b0207687ce54e0e982cfefed)

--- Sampoerna.EMS.BLL/LACK1BLL.cs
+++ Sampoerna.EMS.BLL/LACK1BLL.cs
@@ -3039,6 +3039,16 @@ namespace Sampoerna.EMS.BLL
                 totalUsage = (-1) * getInventoryMovementByParamOutput.IncludeInCk5List.Sum(d => d.ConvertedQty);
             }
 
+            decimal mvt201;
+            if (getInventoryMovementByParamOutput.Mvt201List.Count == 0)
+            {
+                mvt201 = 0;
+            }
+            else
+            {
+                mvt201 = (-1) * getInventoryMovementByParamOutput.Mvt201List.Sum(d => d.ConvertedQty);
+            }
+
             //nebeng in tis to fa field
             //set to tis to fa
             rc.InventoryProductionTisToFa.InvetoryMovementData = new Lack1GeneratedInventoryMovementDto
@@ -3050,7 +3060,7 @@ namespace Sampoerna.EMS.BLL
                 InvMovementAllList =
                     Mapper.Map<List<Lack1GeneratedTrackingDto>>(getInventoryMovementByParamOutput.AllUsageList)
             };
-            rc.TotalUsage = totalUsage;
+            rc.TotalUsage = totalUsage - mvt201;
 
             invMovementOutput = getInventoryMovementByParamOutput;
 
@@ -3160,6 +3170,19 @@ namespace Sampoerna.EMS.BLL
                 //var totalUsageIncludeCk5 = (-1) * getInventoryMovementByParamOutput.IncludeInCk5List.Sum(d => d.QTY.HasValue ? (!string.IsNullOrEmpty(d.BUN) && d.BUN.ToLower() == "kg" ? d.QTY.Value * 1000 : d.QTY.Value) : 0);
                 //totalUsage = totalUsageIncludeCk5;
             }
+
+            decimal mvt201;
+            if (getInventoryMovementByParamOutput.Mvt201List.Count == 0)
+            {
+                mvt201 = 0;
+            }
+            else
+            {
+                mvt201 = (-1) * getInventoryMovementByParamOutput.Mvt201List.Sum(d => d.ConvertedQty);
+            }
+
+            totalUsage = totalUsage - mvt201;
+
             if (isForTisToTis)
             {
                 //set to tis to tis
@@ -3239,6 +3262,8 @@ namespace Sampoerna.EMS.BLL
 
             var movementUsageAll = _inventoryMovementService.GetUsageByParam(usageParamInput);
             var movementUsaheAllWithConvertion = InvMovementConvertionProcess(movementUsageAll, bkcUomId);
+            var movement201 = _inventoryMovementService.GetMvt201(usageParamInput);
+            var movement201WithConvertion = InvMovementConvertionProcess(movement201, bkcUomId);
             var receiving = _inventoryMovementService.GetReceivingByParam(receivingParamInput);
             //get prev receiving for CASE 2 : prev Receiving, Current Receiving, Current Usage
             var prevReceiving = _inventoryMovementService.GetReceivingByParam(prevReceivingParamInput);
@@ -3271,7 +3296,8 @@ namespace Sampoerna.EMS.BLL
                 ReceivingList = receivingList,
                 AllUsageList = movementUsaheAllWithConvertion,
                 ExcludeFromCk5List = movementExclueInCk5List,
-                UsageProportionalList = usageProportionalList
+                UsageProportionalList = usageProportionalList,
+                Mvt201List = movement201WithConvertion
             };
 
             return rc;
