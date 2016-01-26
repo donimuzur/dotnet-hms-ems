60a4c06b2c0e8537ed6f850d5447980038a6a6d8
Yuniar Prasetyo <tyo@voxteneo.co.id>
2016-01-26 17:17:10 +0700

    add new menu of reversal

---
+++ Sampoerna.EMS.Website/Controllers/ReversalController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sampoerna.EMS.Contract;
+using Sampoerna.EMS.Core;
+
+namespace Sampoerna.EMS.Website.Controllers
+{
+    public class ReversalController : BaseController
+    {
+        private IReversalBLL _reversalBll;
+        private Enums.MenuList _mainMenu;
+
+        public ReversalController(IPageBLL pageBll, IReversalBLL reversalBll)
+            : base(pageBll, Enums.MenuList.CK4C)
+        {
+            _reversalBll = reversalBll;
+            _mainMenu = Enums.MenuList.CK4C;
+        }
+
+        #region Index
+        #endregion
+    }
+}
---
+++ Sampoerna.EMS.Website/Models/Reversal/ReversalIndexViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sampoerna.EMS.Website.Models.WorkflowHistory;
+
+namespace Sampoerna.EMS.Website.Models.Reversal
+{
+    public class ReversalIndexViewModel : BaseModel
+    {
+        public ReversalIndexViewModel()
+        {
+            Detail = new List<DataReversal>();
+            WorkflowHistory = new List<WorkflowHistoryViewModel>();
+        }
+
+        public string ProductionDate { get; set; }
+        public string PlantWerks { get; set; }
+
+        public SelectList PlantWerksList { get; set; }
+
+        public List<DataReversal> Detail { get; set; }
+        public List<WorkflowHistoryViewModel> WorkflowHistory { get; set; }
+    }
+
+    public class DataReversal
+    {
+        public int ReversalId { get; set; }
+        public int ZaapShiftId { get; set; }
+        public DateTime? ProductionDate { get; set; }
+        public string FaCode { get; set; }
+        public string Werks { get; set; }
+        public Decimal ReversalQty { get; set; }
+    }
+}
