1cec7080c46e403c88aa7d99ede3a17ee118f5e3
radit <radit@voxteneo.co.id>
2016-05-30 14:38:37 +0700

    fixed and tested

--- Sampoerna.EMS.Website/Controllers/CK5Controller.cs
+++ Sampoerna.EMS.Website/Controllers/CK5Controller.cs
@@ -669,7 +669,7 @@ namespace Sampoerna.EMS.Website.Controllers
             
             var vendorInfo = _lfa1Bll.GetById(model.KppbcNo);
             model.KppBcName = vendorInfo.NAME2 + "-" + model.KppbcNo;
-
+            model.KppbcCity = vendorInfo.NAME2;
             if (string.IsNullOrEmpty(goodTypeGroupId))
             {
                 goodtypeenum = null;
@@ -783,7 +783,7 @@ namespace Sampoerna.EMS.Website.Controllers
 
                 var vendorInfo = _lfa1Bll.GetById(model.KppbcNo);
                 model.KppBcName = vendorInfo.NAME2 + "-" + model.KppbcNo;
-
+                model.KppbcCity = vendorInfo.NAME2;
                 model.CorrespondingPlantList = GetCorrespondingPlantList(plantId, ck5Type);
             }
 
@@ -808,7 +808,7 @@ namespace Sampoerna.EMS.Website.Controllers
             }
             var vendorInfo = _lfa1Bll.GetById(model.KppbcNo);
             model.KppBcName = vendorInfo.NAME2 + "-" + model.KppbcNo;
-
+            model.KppbcCity = vendorInfo.NAME2;
             model.CorrespondingPlantList = GetCorrespondingPlantList(plantId, ck5Type);
 
 
