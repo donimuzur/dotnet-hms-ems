e65dc7d4fb4617fb96aa984071a4c958e3f41087
radit <radit@voxteneo.co.id>
2017-04-26 10:07:02 +0700

    edmx

--- Sampoerna.EMS.DAL/EMSEntities.Context.cs
+++ Sampoerna.EMS.DAL/EMSEntities.Context.cs
@@ -129,5 +129,7 @@ namespace Sampoerna.EMS.BusinessObject
         public virtual DbSet<MASTER_DATA_APPROVAL_DETAIL> MASTER_DATA_APPROVAL_DETAIL { get; set; }
         public virtual DbSet<MONTH_CLOSING> MONTH_CLOSING { get; set; }
         public virtual DbSet<MONTH_CLOSING_DOCUMENT> MONTH_CLOSING_DOCUMENT { get; set; }
+        public virtual DbSet<QUOTA_MONITORING> QUOTA_MONITORING { get; set; }
+        public virtual DbSet<QUOTA_MONITORING_DETAIL> QUOTA_MONITORING_DETAIL { get; set; }
     }
 }
