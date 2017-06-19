d3ca8562c6a244628a3a241552eb0e06395d44db
Yuniar Prasetyo <tyo@indovti.com>
2017-06-19 09:59:35 +0700

    fixed summary report on excise credit

--- Sampoerna.EMS.Website/Models/ExciseCredit/ExciseCreditViewModel.cs
+++ Sampoerna.EMS.Website/Models/ExciseCredit/ExciseCreditViewModel.cs
@@ -22,7 +22,7 @@ namespace Sampoerna.EMS.Website.Models.ExciseCredit
         public SelectList TypeList { set; get; }
 
         public ExciseFilterModel Filter { set; get; }
-
+        public ExciseCreditSummaryReportsViewModel ExportModel { get; set; }
 
     }
 
@@ -33,6 +33,18 @@ namespace Sampoerna.EMS.Website.Models.ExciseCredit
         public string Creator { set; get; }
         public string NPPBKC { set; get; }
         public int ExciseCreditType { set; get; }
+    }
+
+    public class ExciseCreditSummaryReportsViewModel
+    {
+        public bool Type { get; set; }
+        public bool SubmitDate { get; set; }
+        public bool Poa { get; set; }
+        public bool NppbkcId { get; set; }
+        public bool ExciseNumber { get; set; }
+        public bool Amount { get; set; }
+        public bool LastUpdate { get; set; }
+        public bool Status { get; set; }
     }
 
 }
