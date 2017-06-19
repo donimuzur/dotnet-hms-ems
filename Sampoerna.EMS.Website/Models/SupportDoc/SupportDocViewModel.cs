9c3e51d71f22253d0ace51e98d4e80d0ebd9caff
radit <radit@voxteneo.co.id>
2017-06-19 10:41:13 +0700

    fixed build

--- Sampoerna.EMS.Website/Models/SupportDoc/SupportDocViewModel.cs
+++ Sampoerna.EMS.Website/Models/SupportDoc/SupportDocViewModel.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using static Sampoerna.EMS.Core.Enums;
+using Sampoerna.EMS.Core;
 
 namespace Sampoerna.EMS.Website.Models.SupportDoc
 {
