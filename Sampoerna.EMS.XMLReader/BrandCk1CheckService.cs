6772bef32dc0e9003e91e9ea64b5a189fe0e20c5
radit <radit@voxteneo.co.id>
2017-06-21 01:32:37 +0700

    scheduler template not yet tested

--- Sampoerna.EMS.XMLReader/BrandCk1CheckService.cs
+++ Sampoerna.EMS.XMLReader/BrandCk1CheckService.cs
@@ -25,6 +25,9 @@ namespace Sampoerna.EMS.XMLReader
         private ICK1Services _ck1Services;
         private IBrandRegistrationService _brandRegistrationService;
         private MessageService _messageService;
+        private IGenericRepository<USER> _repositoryUser;
+        private IGenericRepository<POA_EXCISER> _repositoryPOAExciser;
+        private IGenericRepository<POA_MAP> _repositoryPoaMapRepository;
 
         private static int xMonth = 5;
 
@@ -36,6 +39,10 @@ namespace Sampoerna.EMS.XMLReader
             _ck1Services = new CK1Services(uow, logger);
             _brandRegistrationService = new BrandRegistrationService(uow,logger);
             _messageService = new MessageService(logger);
+
+            _repositoryUser = uow.GetGenericRepository<USER>();
+            _repositoryPOAExciser = uow.GetGenericRepository<POA_EXCISER>();
+            _repositoryPoaMapRepository = uow.GetGenericRepository<POA_MAP>();
         }
 
         public void BrandCheckProcessCk1()
@@ -60,7 +67,8 @@ namespace Sampoerna.EMS.XMLReader
 
             //Dictionary<ZAIDM_EX_BRAND,List<CK1>> dictBrandCk1 = new Dictionary<ZAIDM_EX_BRAND, List<CK1>>();
             List<InvalidBrandByCk1ForEmail> invalidCk1List = new List<InvalidBrandByCk1ForEmail>();
-
+            
+            var controllers = GetControllers();
             foreach (var brandInvalid in brandNameSoonInvalid)
             {
 
@@ -98,13 +106,135 @@ namespace Sampoerna.EMS.XMLReader
                     };
 
                     invalidCk1List.Add(invalidCk1);
+                    invalidCk1.userCc = controllers;
+                    var listPlantBrand = brandList.Select(x => x.WERKS).Distinct().ToList();
+                    invalidCk1.userTo = GetPoaExcisersByPlant(listPlantBrand);
                 }
             }
+
+            ProcessDataToEmail(invalidCk1List);
         }
 
         private void ProcessDataToEmail(List<InvalidBrandByCk1ForEmail> invalidCk1List)
         {
             //do email things here
+            foreach (var invalidBrandByCk1ForEmail in invalidCk1List)
+            {
+                
+                if (invalidBrandByCk1ForEmail.userTo.Count > 0)
+                {
+                    var success = false;
+                    var mailNotif = ProcessEmailQuotaNotification(invalidBrandByCk1ForEmail);
+                    if (mailNotif != null)
+                    {
+                        if (mailNotif.IsCCExist)
+                        {
+                            success = _messageService.SendEmailToListWithCC(mailNotif.To, mailNotif.CC, mailNotif.Subject, mailNotif.Body, false);
+                        }
+                        else
+                        {
+                            success = _messageService.SendEmailToList(mailNotif.To, mailNotif.Subject, mailNotif.Body, false);
+                        }
+                        var emailStatus = success ? Core.Enums.EmailStatus.Sent : Core.Enums.EmailStatus.NotSent;
+                        
+                    }
+                }
+            }
         }
+
+        private MailNotification ProcessEmailQuotaNotification(InvalidBrandByCk1ForEmail invalidCk1)
+        {
+            var bodyMail = new StringBuilder();
+            var rc = new MailNotification();
+
+            rc.Subject = "Brand " + invalidCk1.BrandName + "Never used on any CK1 in the last 5 months.";
+            bodyMail.Append("Dear Team,<br />");
+
+            bodyMail.Append("Kindly be informed, " + rc.Subject + ". <br />");
+
+            bodyMail.Append(BuildBodyMailForQuotaNotification(invalidCk1));
+
+            rc.To.AddRange(invalidCk1.userTo.Select(x => x.EMAIL).ToList());
+
+            rc.Body = bodyMail.ToString();
+            foreach (var controller in invalidCk1.userCc)
+            {
+                rc.IsCCExist = true;
+                rc.CC.Add(controller.EMAIL);
+            }
+
+            return rc;
+        }
+
+        private string BuildBodyMailForQuotaNotification(InvalidBrandByCk1ForEmail invalidCk1)
+        {
+            var bodyMail = new StringBuilder();
+            //var supplier = dataMonitoring.SUPPLIER_WERKS;
+            bodyMail.Append("<table><thead>" +
+                            "<tr>" +
+                            "<td>Fa Code </td>" +
+                            "<td>Plant</td>" +
+                            "<td>Sticker Code</td>" +
+                            "<td>CK1 Number</td>" +
+                            "<td>Ck1 Date</td>" +
+                            "</tr></thead>");
+            foreach (var brand in invalidCk1.BrandList)
+            {
+                bodyMail.Append("<tr>" +
+                                "<td>" + brand.FaCode + "</td>" +
+                                "<td>" + brand.Werks + "</td>" +
+                                "<td>" + brand.StickerCode + "</td>");
+                if (brand.LastCk1 != null)
+                {
+                    bodyMail.Append(
+                        "<td>" + brand.LastCk1.CK1_NUMBER + "</td>" +
+                        "<td>" + brand.LastCk1.ORDER_DATE + "</td>" +
+                        "</tr>");
+                }
+                else
+                {
+                    bodyMail.Append(
+                        "<td colspan='2'>No Ck1 Data found</td>" +
+                        
+                        "</tr>");
+                }
+            }
+            
+            
+
+
+            bodyMail.Append("</table>");
+            bodyMail.AppendLine();
+            bodyMail.Append("<br />Regards,<br />");
+
+            return bodyMail.ToString();
+        } 
+
+        private List<USER> GetControllers()
+        {
+            var controllersList = new List<USER>();
+
+            var data = _repositoryUser.Get(x => (!x.IS_ACTIVE.HasValue || x.IS_ACTIVE != 0) && x.BROLE_MAP.Any(y => y.ROLEID == Sampoerna.EMS.Core.Enums.UserRole.Controller && y.MSACCT.ToLower() == x.USER_ID.ToLower()), null, "BROLE_MAP").ToList();
+
+
+            controllersList.AddRange(data);
+            return controllersList;
+        }
+
+        private List<USER> GetPoaExcisersByPlant(List<string> werks)
+        {
+            var excisersList = new List<USER>();
+
+            var data = _repositoryPOAExciser.Get(null, null, "").Select(x=> x.POA_ID.ToUpper()).ToList();
+            var poaByPlant =
+                _repositoryPoaMapRepository.Get(x => werks.Contains(x.WERKS) && data.Contains(x.POA_ID.ToUpper()), null, "")
+                    .Select(x => x.POA_ID.ToUpper())
+                    .ToList();
+            excisersList = _repositoryUser.Get(x => poaByPlant.Contains(x.USER_ID), null, "").ToList();
+
+            return excisersList;
+        }
+
+        
     }
 }
