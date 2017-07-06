145f23f6bf144ad942d3319e5dc58504b27f73a3
radit <radit@voxteneo.co.id>
2017-07-06 15:10:40 +0700

    update compile error

--- Sampoerna.EMS.Website/Models/BrandRegistrationTransaction/ProductDevelopment/ProductDevelopmentViewModel.cs
+++ Sampoerna.EMS.Website/Models/BrandRegistrationTransaction/ProductDevelopment/ProductDevelopmentViewModel.cs
@@ -9,8 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using static Sampoerna.EMS.Core.Enums;
-//using static Sampoerna.EMS.Core.Enums;
+
 
 namespace Sampoerna.EMS.Website.Models.BrandRegistrationTransaction.ProductDevelopment
 {
@@ -25,7 +24,7 @@ namespace Sampoerna.EMS.Website.Models.BrandRegistrationTransaction.ProductDevel
             this.SearchInput = new ProductDevFilterViewModel();        
         }
         public int TempAction { get; set; }
-        public ProductDevelopmentAction productAction { get; set; }
+        public Enums.ProductDevelopmentAction productAction { get; set; }
         public List<ProductDevelopmentModel> ListProductDevelopment { get; set; }
         public ProductDevelopmentModel ViewModel { get; set; }
 
--- Sampoerna.EMS.Website/Models/SupportDoc/SupportDocViewModel.cs
+++ Sampoerna.EMS.Website/Models/SupportDoc/SupportDocViewModel.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using static Sampoerna.EMS.Core.Enums;
+
 
 namespace Sampoerna.EMS.Website.Models.SupportDoc
 {
